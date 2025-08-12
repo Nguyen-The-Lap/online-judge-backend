31ac1a1ebfe8f8e4f9b6056c9c3cc73a7ad779ba
Nguyen-The-Lap <91041708+Nguyen-The-Lap@users.noreply.github.com>
2025-08-12 11:32:56 +0700

    Update corsOptions.js

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,15 +1,17 @@
-const allowedOrigins = require("./allowedOrigins");
+const allowedOrigins = process.env.ALLOWED_ORIGINS
+  ? process.env.ALLOWED_ORIGINS.split(",").map(origin => origin.trim())
+  : [];
 
 const corsOptions = {
   origin: (origin, callback) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
   credentials: true,
-  optionsSuccessStatus: 200,
+  optionsSuccessStatus: 200
 };
 
 module.exports = corsOptions;
